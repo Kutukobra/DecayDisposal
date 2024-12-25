1ebf8bb4500022732a0e1a9f881fed23464d3f06
Kutukobra <nadzhiff@gmail.com>
2024-12-25 10:15:39 +0700

    Enemy Roaming

--- Assets/Scripts/Entity/EnemyAI.cs
+++ Assets/Scripts/Entity/EnemyAI.cs
@@ -7,8 +7,12 @@ public class EnemyAI : MonoBehaviour
     private Vector2 startingPosition;
 
     public float roamRadius = 10;
+    public float movementSpeed = 5f;
 
     Rigidbody2D rigidBody;
+
+    private Vector2 roamingPosition;
+    public float roamInterval = 10f;
 
     public void Start()
     {
@@ -21,8 +25,22 @@ public class EnemyAI : MonoBehaviour
         return startingPosition + Direction2D.GetRandomDirectionf() * Random.Range(roamRadius / 10, roamRadius);
     }
 
+    private float nextTime = 0f;
     public void Update()
     {
+        if (Time.time > nextTime)
+        {
+            roamingPosition = GetRoamingPosition();
+            nextTime = Time.time + roamInterval;
+        }
 
+        if (Vector2.Distance(transform.position, roamingPosition) < 2)
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
+        else
+        {
+            rigidBody.velocity = (roamingPosition - (Vector2)transform.position).normalized * movementSpeed;
+        }
     }
 }
